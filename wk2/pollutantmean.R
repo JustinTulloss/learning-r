f08dfb7af86363b2056a55ac6730cb2d84d92976
Justin Tulloss <justin.tulloss@gmail.com>
2014-04-17 15:46:33 -0700

    need to round the answer

--- wk2/pollutantmean.R
+++ wk2/pollutantmean.R
@@ -5,5 +5,6 @@ pollutantmean <- function(dir, pollutant, id = 1:332) {
       readings <- !is.na(d[,pollutant])
       c(sum(d[readings, pollutant]), sum(readings))
     })
-    sum(sums[1,]) / sum(sums[2,])
+    mean <- sum(sums[1,]) / sum(sums[2,])
+    round(mean, 3)
 }
