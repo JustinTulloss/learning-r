bed38d23ea0adc0ef728a6be3f3d43d4a0ac51de
Justin Tulloss <justin.tulloss@gmail.com>
2014-04-21 14:32:15 -0700

    Problem 2 solution
    - Factored out function to read the data in
    - Implemented "complete", which returns a data frame that has two
      columns, one for the id and one for the number of complete cases

--- wk2/pollutantmean.R
+++ wk2/pollutantmean.R
@@ -1,10 +1,21 @@
-pollutantmean <- function(dir, pollutant, id = 1:332) {
+readdata <- function(dir, id) {
     files <- sapply(id, function(i) { sprintf("%s/%03d.csv", dir, i) })
-    data <- lapply(files, read.csv)
+    lapply(files, read.csv)
+}
+
+pollutantmean <- function(dir, pollutant, id = 1:332) {
+    data <- readdata(dir, id)
     sums <- sapply(data, function(d) {
       readings <- !is.na(d[,pollutant])
       c(sum(d[readings, pollutant]), sum(readings))
     })
     mean <- sum(sums[1,]) / sum(sums[2,])
     round(mean, 3)
+}
+
+complete <- function(dir, id = 1:332) {
+    data <- readdata(dir, id)
+    cases <- lapply(data, complete.cases)
+    sums <- sapply(cases, sum)
+    data.frame(id = id, nobjs = sums)
 }
